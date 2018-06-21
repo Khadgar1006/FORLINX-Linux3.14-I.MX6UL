1efae47614dfc94002c5a1558b03fd28255ecee4
khadgar <yjtyzw@126.com>
2018-06-21 13:01:08 -0400

    compile completely

--- linux-3.14.38/build.sh
+++ linux-3.14.38/build.sh
@@ -4,10 +4,8 @@ export CPUS=`grep -c processor /proc/cpuinfo`
 export ARCH=arm
 export CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
 export PATH=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin:$PATH
-make distclean
 
-para=$1
-echo "build for $para"
+make distclean
 if [ "$1" == "emmc" ]; then
     cp ./linux_imx6ul_emmc_config .config
 elif [ "$1" == "nand" ]; then
