4cdbf422dcb0f8996f3dcfe21e28b2e3bd5388de
khadgar <yjtyzw@126.com>
2018-06-25 14:50:29 +0800

    git case captital and small letter

--- linux-3.14.38/build.sh
+++ linux-3.14.38/build.sh
@@ -5,18 +5,23 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi-
 export PATH=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin:$PATH
 
-make distclean
-if [ "$1" == "emmc" ]; then
-	echo "build emmc"
-    cp ./linux_imx6ul_emmc_config .config
-elif [ "$1" == "nand" ]; then
-	echo "build nand"
-    cp ./linux_imx6ul_nand_config .config
-else
-	echo "build emmc"
-    cp ./linux_imx6ul_emmc_config .config
-fi
 
-make zImage -j${CPUS}
-make dtbs
-make modules
+if [ "$1" == "zImage" ]; then
+	cp ./linux_imx6ul_emmc_config .config
+    make zImage -j${CPUS}
+elif [ "$1" == "dtbs" ]; then
+	cp ./linux_imx6ul_emmc_config .config
+	make dtbs
+elif [ "$1" == "modules" ]; then
+	cp ./linux_imx6ul_emmc_config .config
+	make modules
+elif [ "$1" == "all" ]; then
+	make distclean
+	echo "build emmc"
+    cp ./linux_imx6ul_emmc_config .config
+    make zImage -j${CPUS}
+	make dtbs
+	make modules
+else
+	echo "parameter error"
+fi
